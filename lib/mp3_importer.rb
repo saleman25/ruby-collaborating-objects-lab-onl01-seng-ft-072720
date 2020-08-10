e0c2fbb785306418c7b68bd813a32ba2c6e707fe
Samantha Aleman <samantha.aleman25@gmail.com>
2020-08-10 20:00:19 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -14,7 +14,7 @@ end
 def import  
   self.files.each do |file|
     Song.new_by_filename(file)
-    Artist.all << song.artist unless Artist.all.include?(song)
+    Artist.all << song.artist unless Artist.all.include?(song.artist)
   end 
 end   
   
