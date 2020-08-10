90e5b83926e06646fe8aadfb43ba471b2fa96c4e
Samantha Aleman <samantha.aleman25@gmail.com>
2020-08-10 19:49:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,6 +15,7 @@ def self.new_by_filename(file_name)
   split_name = file_name.split" - "
   song = self.new(split_name[1])
   artist = Artist.find_or_create_by_name(split_name[0]) 
+  song.artist = artist
 end  
  
 def artist_name(name) 
