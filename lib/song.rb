89984f950a6994f1b4299984fe1ebb574d44d298
Samantha Aleman <samantha.aleman25@gmail.com>
2020-08-10 18:51:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,14 +2,16 @@ class Song
   
 attr_accessor :name , :artist
 
+@@all = []
  
 def initialize(name)
   @name = name 
   @artist = artist
+  @@all
 end 
   
 def self.all 
-   
+  @@all  
 end 
   
 def self.new_by_filename
