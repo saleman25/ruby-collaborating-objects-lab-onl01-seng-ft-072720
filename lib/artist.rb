cce5e4215827ebdd6eba75ddfc88eae9ca9f8736
Samantha Aleman <samantha.aleman25@gmail.com>
2020-08-10 18:39:02 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -8,8 +8,22 @@ def initialize(name)
   @name = name 
 end 
   
-  
-  
+def self.all 
+  @@all 
+end 
+
+def add_song 
+  @all << song
+end 
+
+def songs 
+  song.all.collect 
+end 
+
+def self.find_or_create_by_name 
+end 
+
+def prints_song
   
   
   
