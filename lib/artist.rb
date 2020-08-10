f24f4881bc2b65861e54f6a90315f3bbbcdd7bba
Samantha Aleman <samantha.aleman25@gmail.com>
2020-08-10 19:34:46 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -23,7 +23,7 @@ def songs(song)
 end 
 
 def self.find_or_create_by_name 
-  self.all.detect {|artist| artist.name == name } || Artist.new
+  self.all.detect {|artist| artist.name == name } || Artist.new(name)
 end 
 
 def prints_song
